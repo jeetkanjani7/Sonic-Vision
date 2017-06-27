9aa534cfea9ed6cd4a60403dfb338826e3412adc
jeetkanjani7 <jeetkanjani7@gmail.com>
2017-06-27 16:19:59 +0100

    first

---
+++ Assets/Scripts/Gaze.cs
@@ -0,0 +1,131 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using UnityEngine.UI;
+using HoloToolkit.Unity;
+
+namespace Academy.HoloToolkit.Unity
+{
+
+    /// <summary>
+    /// GazeManager determines the location of the user's gaze, hit position and normals.
+    /// </summary>
+    public class Gaze : MonoBehaviour//Singleton<Gaze>
+    {
+        public static bool placing =true;
+        private float timer = 0;
+
+        private float timerMax = 0;
+
+
+        [Tooltip("Maximum gaze distance, in meters, for calculating a hit.")]
+        public float MaxGazeDistance = 15.0f;
+
+        [Tooltip("Select the layers raycast should target.")]
+        public LayerMask RaycastLayerMask = Physics.DefaultRaycastLayers;
+
+        /// <summary>
+        /// Physics.Raycast result is true if it hits a Hologram.
+        /// </summary>
+        public bool Hit { get; private set; }
+
+        /// <summary>
+        /// HitInfo property gives access
+        /// to RaycastHit public members.
+        /// </summary>
+        public RaycastHit HitInfo { get; private set; }
+
+        /// <summary>
+        /// Position of the intersection of the user's gaze and the hologram's in the scene.
+        /// </summary>
+        public Vector3 Position { get; private set; }
+
+        /// <summary>
+        /// RaycastHit Normal direction.
+        /// </summary>
+        public Vector3 Normal { get; private set; }
+
+        private Vector3 gazeOrigin;
+        private Vector3 gazeDirection;
+        private float lastHitDistance = 15.0f;
+
+        private void Update()
+        {
+            GameObject holocam = GameObject.Find("HololensCamera");
+            placing = GazeGestureManager.placingmode;
+
+            gazeOrigin = Camera.main.transform.position;
+            gazeDirection = Camera.main.transform.forward;
+            UpdateRaycast();
+        }
+
+        /// <summary>
+        /// Calculates the Raycast hit position and normal.
+        /// </summary>
+        private void UpdateRaycast()
+        {
+            // Get the raycast hit information from Unity's physics system.
+            RaycastHit hitInfo;
+            Hit = Physics.Raycast(gazeOrigin,
+                           gazeDirection,
+                           out hitInfo,
+                           MaxGazeDistance,
+                           RaycastLayerMask);
+
+            // Update the HitInfo property so other classes can use this hit information.
+            HitInfo = hitInfo;
+
+            if (Hit && placing==false)
+            {
+                // If the raycast hits a hologram, set the position and normal to match the intersection point.
+                Position = hitInfo.point;
+                Normal = hitInfo.normal;
+                lastHitDistance = hitInfo.distance;
+                //  yield return new WaitForSeconds(3.0f);
+                //   if (!Waited(1)) return;
+                gameObject.SetActive(false);
+                //RandomObjects other = (RandomObjects)cam.GetComponent(typeof(RandomObjects));
+               // GameObject cursor = GameObject.Find("Cursor");
+
+                GameObject cam = GameObject.Find("HoloLensCamera");
+                cam.GetComponent<AudioSource>().Play();
+                //cursor.SetActive(true);
+                //cursor.GetComponent<CursorManager>().LateUpdate();
+                cam.GetComponent<RandomObjects>().objectinit();
+
+
+
+                //   cam.GetComponent<RandomObjects>().selectRandom();
+
+            }
+            else
+            {
+                // If the raycast does not hit a hologram, default the position to last hit distance in front of the user,
+                // and the normal to face the user.
+                Position = gazeOrigin + (gazeDirection * lastHitDistance);
+                Normal = gazeDirection;
+            }
+        }
+        private bool Waited(float seconds)
+        {
+            timerMax = seconds;
+
+            timer += Time.deltaTime;
+
+            if (timer >= timerMax)
+            {
+                return true; //max reached - waited x - seconds
+            }
+
+            return false;
+        }
+
+        IEnumerator Example()
+        {
+
+            yield return new WaitForSeconds(3);
+
+        }
+
+    }
+}
---
+++ Assets/Scripts/RandomObjects.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomObjects : MonoBehaviour
+{
+    public GameObject start;
+    private float timer = 0;
+
+    private float timerMax = 0;
+    public static int cnt = 0;
+
+    public GameObject sphere1,sphere2, cylinder2, cylinder1, bike, tree;
+    public static List<GameObject> objs = new List<GameObject>();
+    
+    void Start()
+    {
+
+
+
+        Vector3 rndPos = new Vector3(Random.Range(-20, 20), Random.Range(-20, 20), Random.Range(-20, 20));
+        
+        objs.Add(sphere1);
+        objs.Add(cylinder2);
+        
+        objs.Add(bike);
+        objs.Add(tree);
+        objs.Add(sphere2);
+        objs.Add(cylinder1);
+
+
+
+    }
+
+    public static void appstart()
+    {
+        objs[cnt].SetActive(true);
+        objs[cnt].GetComponent<AudioSource>().Play();
+        cnt++;
+        for (int i = 1; i <= objs.Count - 1; i++)
+        {
+            objs[i].SetActive(false);
+        }
+    }
+
+    public void objectinit()
+    {
+        StartCoroutine(selectRandom());
+    }
+
+    public IEnumerator selectRandom()
+    {
+
+
+        int len = objs.Count;
+       
+     
+        if (cnt < objs.Count)
+        {
+            yield return new WaitForSeconds(3);
+            objs[cnt].SetActive(true);
+            objs[cnt].GetComponent<AudioSource>().Play();
+            cnt++;
+        }
+        else
+        {
+            GameObject cam = GameObject.Find("HoloLensCamera");
+            cam.GetComponent<AudioSource>().Play();
+            cnt = 0;
+            
+            start.SetActive(true);
+        }
+
+    }
+    /*
+    private bool Waited(float seconds)
+    {
+        timerMax = seconds;
+
+        timer += Time.deltaTime;
+
+        if (timer >= timerMax)
+        {
+            return true; //max reached - waited x - seconds
+        }
+
+        return false;
+    }
+
+    IEnumerator Example()
+    {
+
+        yield return new WaitForSeconds(3);
+
+    }
+    */
+}
